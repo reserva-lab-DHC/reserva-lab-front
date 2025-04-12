38f02bc586b64036fc8d126667bf60dd16753f2b
Lucas Lopes <lucasslopes2004@gmail.com>
2025-04-12 16:31:03 -0300

    feat: header and footer component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,18 @@
-import { Component, input } from '@angular/core';
+import { Component, input, OnInit, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './features/header/header.component';
+import { FooterComponent } from "./features/footer/footer.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  standalone: true,
+  imports: [RouterOutlet, HeaderComponent, FooterComponent],
+  styleUrls: ['./app.component.scss'],
+  templateUrl: './app.component.html'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
+  ngOnInit(): void {
+  }
   title = 'reserva-lab-front';
 }
