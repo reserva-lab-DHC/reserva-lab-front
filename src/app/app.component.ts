898c743895a4e173333813dfcb570e60d308297c
ulisses <76224942+Lulki@users.noreply.github.com>
2025-05-29 20:27:21 -0300

    Criação do card de confirmar solicitações e a própria página de solicitações

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,12 +2,13 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./shared/header/header.component";
 import { FooterComponent } from "./shared/footer/footer.component";
-
+import { ConfirmarSolicitacoesComponent } from './features/confirmar-solicitacoes/confirmar-solicitacoes.component';
+import { ConfirmRequestComponent } from './shared/confirm-request/confirm-request.component';
 
 @Component({
   selector: 'dhc-app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, FooterComponent],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent, ConfirmarSolicitacoesComponent, ConfirmRequestComponent],
   styleUrls: ['./app.component.scss'],
   templateUrl: './app.component.html'
 })
---
+++ src/app/features/confirmar-solicitacoes/confirmar-solicitacoes.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { ConfirmRequestComponent } from "../../shared/confirm-request/confirm-request.component";
+
+@Component({
+  selector: 'dhc-confirmar-solicitacoes',
+  imports: [ConfirmRequestComponent],
+  templateUrl: './confirmar-solicitacoes.component.html',
+  styleUrl: './confirmar-solicitacoes.component.scss'
+})
+export class ConfirmarSolicitacoesComponent {
+
+}
