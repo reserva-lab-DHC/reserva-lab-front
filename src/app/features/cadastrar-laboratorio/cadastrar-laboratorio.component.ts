fa44c3d6b16c26effff05997803b3178b9138926
Lucas Lopes <lucasslopes2004@gmail.com>
2025-05-31 12:46:42 -0300

    fix select component

--- src/app/features/cadastrar-laboratorio/cadastrar-laboratorio.component.ts
+++ src/app/features/cadastrar-laboratorio/cadastrar-laboratorio.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
+import { SelecaoComponent } from "../../shared/componente-selecao/selecao.component";
 
 @Component({
   selector: 'dhc-cadastrar-laboratorio',
-  imports: [],
+  imports: [SelecaoComponent],
   templateUrl: './cadastrar-laboratorio.component.html',
   styleUrl: './cadastrar-laboratorio.component.scss'
 })
 export class CadastrarLaboratorioComponent {
-
+  predioSelecionado: number = 0;
+  andarSelecionado: number = 0;
 }
--- src/app/shared/componente-selecao/selecao.component.ts
+++ src/app/shared/componente-selecao/selecao.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Component, Input, Output, EventEmitter, input, output, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 @Component({
@@ -9,22 +9,22 @@ import { CommonModule } from '@angular/common';
   styleUrls: ['./selecao.component.scss']
 })
 export class SelecaoComponent {
-  @Input() titulo = '';
-  @Input() opcoes: number[] = [];
-  @Input() selecionado: number | null = null;
-  @Input() desabilitar: number[] = [];
-  @Input() layout = 'horizontal';
 
-  @Output() selecionadoChange = new EventEmitter<number>();
+  titulo = input<string>('');
+  opcoes = input<number[]>([]);
+  selecionado = signal<number | null>(null);
+  desabilitar = input<number[]>([]);
+  layout = input<'horizontal' | 'vertical'>('horizontal');
+  selecionadoChange = output<number>();
 
   selecionar(valor: number): void {
-    if (this.selecionado !== valor && !this.desabilitar.includes(valor)) {
-      this.selecionado = valor;
+    if (this.selecionado() !== valor && !this.desabilitar().includes(valor)) {
+      this.selecionado.set(valor);
       this.selecionadoChange.emit(valor);
     }
   }
 
   estaDesabilitado(valor: number): boolean {
-    return this.desabilitar.includes(valor);
+    return this.desabilitar().includes(valor);
   }
 }
