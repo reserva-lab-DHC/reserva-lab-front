3ba996511c4699e57c06eff2f014cde98ecb466d
AnaSouzaPinheiro <anapinheirole@gmail.com>
2025-06-01 19:57:44 -0300

    feat calendario

---
+++ src/app/shared/calendar/calendar/calendar.component.ts
@@ -0,0 +1,17 @@
+import { Component, input } from '@angular/core';
+import { DatePickerModule } from 'primeng/datepicker';
+import { FormsModule } from '@angular/forms';
+
+export type Mode = 'range' | 'multiple' | 'single';
+
+@Component({
+  selector: 'dhc-calendar',
+  imports: [ DatePickerModule, FormsModule ],
+  templateUrl: './calendar.component.html',
+  styleUrl: './calendar.component.scss'
+})
+
+export class CalendarComponent {
+  dates: Date[] = [];
+  selectionMode = input<Mode>('single');
+}
