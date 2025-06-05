470ecb0c1bc0152bccb6505e63dc830e31fa4b55
Lucas Lopes <lucasslopes2004@gmail.com>
2025-06-04 22:01:40 -0300

    ## esqueci de subir coisas

--- src/app/features/solicitacao-de-reservas/solicitacao-de-reservas.component.ts
+++ src/app/features/solicitacao-de-reservas/solicitacao-de-reservas.component.ts
@@ -19,9 +19,11 @@ export class SolicitacaoDeReservasComponent  {
   //todo : implementar lógica de solicitação de reservas
   // basicamente falta:
   // - criar o serviço de solicitação de reservas
-  // - formgroup para o formulário de solicitação
+  // - formgroup para o formulário de solicitação incluindo o botão de horário
   // - implementar a lógica de envio do formulário
   // - implementar um sinal que basicamente controlar se um input aparece ou não (o que valida o tipo de usuário, se é aluno ou professor)
+  // - Fernanda, caso vc n tenha lembrdom, a Ana chegou a comentar ocm vc que fez alteraçoes na task do ghit e colocou um print e um link pro figma de como seria o visual dos botões de horário,
+  // e também colocou um link do primeng para como fazer o grupo de botões
 
 }
 
