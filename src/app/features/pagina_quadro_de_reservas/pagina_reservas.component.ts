7af40ddaef09c8b37af962c3d4b4cc353c8d2dc8
ulisses <76224942+Lulki@users.noreply.github.com>
2025-06-08 11:37:44 -0300

    Remover declarações não usadas

--- src/app/features/pagina_quadro_de_reservas/pagina_reservas.component.ts
+++ src/app/features/pagina_quadro_de_reservas/pagina_reservas.component.ts
@@ -1,9 +1,7 @@
 import { CommonModule } from '@angular/common';
-import { DailyScheduleHeaderComponent } from '../../shared/daily-schedule-header/daily-schedule-header.component';
 import { ScheduleTableComponent } from '../../shared/schedule-table/schedule-table.component';
 import { Component } from '@angular/core';
 import { HeaderQuadroComponent } from "../../shared/header-quadro/header-quadro.component";
-import { HeaderComponent } from "../../shared/header/header.component";
 
 @Component({
   selector: 'dhc-pagina-reservas', // Confirme se esse é o seletor correto utilizado na rota.
@@ -11,10 +9,8 @@ import { HeaderComponent } from "../../shared/header/header.component";
   imports: [
     CommonModule,
     // ModalsComponent, // Descomente se for utilizado no HTML.
-    DailyScheduleHeaderComponent,
     ScheduleTableComponent,
-    HeaderQuadroComponent,
-    HeaderComponent
+    HeaderQuadroComponent
 ],
   templateUrl: './pagina_reservas.component.html',
   styleUrls: ['./pagina_reservas.component.scss']
