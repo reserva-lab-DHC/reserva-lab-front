df5a3db8fbfbeadc4499e8e5e38949ef91520a23
Lucas Lopes <lucasslopes2004@gmail.com>
2025-06-17 20:31:00 -0300

    pq isso tava ali asduahsdu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,12 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./shared/header/header.component";
 import { FooterComponent } from "./shared/footer/footer.component";
-import { QuadroDeReservasComponent } from "./features/quadro-de-reservas/quadro-de-reservas.component";
 
 @Component({
   selector: 'dhc-app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, FooterComponent, QuadroDeReservasComponent],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent],
   styleUrls: ['./app.component.scss'],
   templateUrl: './app.component.html'
 })
