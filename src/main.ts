9065e592788064083ad5b530805cb1e48cd0d448
ulisses <76224942+Lulki@users.noreply.github.com>
2025-06-21 14:04:02 -0300

    fix: erro ao iniciar a aplicação

--- src/main.ts
+++ src/main.ts
@@ -3,20 +3,15 @@ import { AppComponent } from './app/app.component';
 import { provideRouter } from '@angular/router';
 import { routes } from './app/app.routes';
 import { provideAnimations } from '@angular/platform-browser/animations';
-
-bootstrapApplication(AppComponent, {
-  providers: [
-    provideRouter(routes),
-    provideAnimations()
-  ]
-}).catch(err => console.error(err));
+import { provideHttpClient } from '@angular/common/http';
 
 console.log('Inicializando app...');
 
 bootstrapApplication(AppComponent, {
   providers: [
     provideRouter(routes),
-    provideAnimations()
+    provideAnimations(),
+    provideHttpClient()
   ]
 }).catch(err => {
   console.error('Erro no bootstrapApplication', err);
