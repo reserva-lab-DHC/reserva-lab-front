2c4017caf2d1629da3700c55af89d13a62189c3f
ulisses <76224942+Lulki@users.noreply.github.com>
2025-06-22 17:33:28 -0300

    lint warning

--- src/app/shared/header-quadro/header-quadro.component.ts
+++ src/app/shared/header-quadro/header-quadro.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, EventEmitter, OnDestroy, OnInit, Output } from '@angular/core';
 import { DropdownComponent } from "../dropdown/dropdown.component";
 import { NgIf } from '@angular/common';
 import { CalendarComponent } from "../calendar/calendar/calendar.component";
@@ -11,7 +11,7 @@ import { RecentRequestComponent } from "../modals/ui/modal-recent-requests/modal
   styleUrl: './header-quadro.component.scss'
 })
 
-export class HeaderQuadroComponent {
+export class HeaderQuadroComponent implements OnInit, OnDestroy {
   showCalendar = false
   showPopup = false;
   date = new Date()
